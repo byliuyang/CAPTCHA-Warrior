1949be639886a735ff71a42b6a935ce867d51f56
svadlamudi <skvadlamudi@live.com>
2017-12-09 13:41:55 -0500

    Implement calAccuracy
    The function that computes accuracy given the predicted and expected
    results works.

--- calAccuracy.m
+++ calAccuracy.m
@@ -1,4 +1,18 @@
 function accuracy = calAccuracy(predictedResults, expectedResults)
-    % Return accuracy based on the predicted results and the expected
-    % results
+    % calAccuracy Calculate accuracy of predicted results given expected
+    % Inputs:
+    %   predictedResults Cell array of predicted label of CAPTCHA
+    %   expectedResults Cell array of expected label of CAPTCHA
+    % Outputs:
+    %   accuracy Percent of predicted labels that are correct
+    
+    num_correct = 0;
+    
+    for i = 1 : length(predictedResults)
+        if strcmp(predictedResults{i}, expectedResults{i})
+            num_correct = num_correct + 1;
+        end
+    end
+    
+    accuracy = (num_correct / length(predictedResults)) * 100;
 end
