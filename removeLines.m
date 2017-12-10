3a2bef28eafdd6ab7ff29d26bfebecacaced540a
Yang Liu <byliuyang11@gmail.com>
2017-12-09 20:22:45 -0500

    Add remove lines and tests

--- removeLines.m
+++ removeLines.m
@@ -1,3 +1,5 @@
 function newImage = removeLines(image)
     % Remove the lines in the image
+    SE = strel('arbitrary', [1 0 1; 0 1 0; 1 0 1]);
+    newImage = imerode(image, SE);
 end
--- run.m
+++ run.m
@@ -1,3 +1,4 @@
+close all;
 % load images and corresponding labels
 [images, labels] = getCaptchsAndLabels();
 % split data into training set and testing set
@@ -5,3 +6,15 @@
 % train the model
 % recognize the CAPTCHAs from the testing set
 % calculate the accuracy
+figure;
+I = trainImages{1};
+subplot(2, 2, 1);
+imshow(I);
+
+I = removeBackgroundColor(I);
+subplot(2, 2, 2);
+imshow(I)
+
+I = removeLines(I);
+subplot(2, 2, 3);
+imshow(I)
